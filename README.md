e2e7cd96c1bc85c9f83d231d8dd57139ecad12be
unknown <neilbeukes8@gmail.com>
2019-06-02 15:22:37 +0200

    removed camera route, hardware error kills application on start-up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Pet Feeder
+#Pet Feeder
+
+Flask Pyhon application that contains basic Rest endpoints for Mobile application coming in future. (Proof of concept)
--- routes.py
+++ routes.py
@@ -5,17 +5,17 @@ from flask_pymongo import PyMongo
 import RPi.GPIO as GPIO
 import time
 import datetime
-from picamera import PiCamera
+#from picamera import PiCamera
 
-camera = PiCamera()
+#camera = PiCamera()
 
 app = Flask(__name__)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(26,GPIO.OUT)
-GPIO.setup(6,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 app.config['MONGO_DBNAME'] = 'pigate'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/feedmypet'
 
@@ -29,40 +29,47 @@ mongo = PyMongo(app)
 #     rgb.setColour(redvalue,greenvalue,bluevalue)
 #     return "Colour set"
 
+
 @app.route('/feed')
 def test():
     global result26
-    if result26 == False :
+    if result26 == False:
         result26 = True
         print("Feeding...")
-        GPIO.output(26,GPIO.HIGH)
+        GPIO.output(26, GPIO.HIGH)
         time.sleep(3)
-        GPIO.output(26,GPIO.LOW)
+        GPIO.output(26, GPIO.LOW)
         print("Done feeding this scrub!")
         result26 = False
         feedmypet = mongo.db.status
-        feedmypet.update_one({'id':'12345'}, {'$set': {'last_fed': datetime.datetime.now(), 'level_perc': 45}})
+        feedmypet.update_one({'id': '12345'}, {
+                             '$set': {'last_fed': datetime.datetime.now(), 'level_perc': 45}})
         output = []
         for s in feedmypet.find():
-            output.append({'id' : s['id'], 'last_fed' : s['last_fed'], 'level' : s['level']})
-        return jsonify({'result' : output})
+            output.append(
+                {'id': s['id'], 'last_fed': s['last_fed'], 'level': s['level']})
+        return jsonify({'result': output})
     else:
         return "Sorry, still busy feeding the scrub!"
 
-@app.route('/camera')
-def cameraPhoto():
-    camera.start_preview()
-    sleep(5)
-    camera.capture('/home/pi/Desktop/image.jpg')
-    camera.stop_preview()
+# Disabling this route for now as my camera does not want to pla along, potentiall a hardware error
+# @app.route('/camera')
+# def cameraPhoto():
+#     camera.start_preview()
+#     sleep(5)
+#     camera.capture('/home/pi/Desktop/image.jpg')
+#     camera.stop_preview()
+#     camera.close()
+
 
 @app.route('/history')
 def testdb():
     feedmypet = mongo.db.history
     output = []
     for s in pigate.find():
-        output.append({'id' : s['id'], 'gate_closed' : s['gate_closed']})
-    return jsonify({'result' : output})
-    
+        output.append({'id': s['id'], 'gate_closed': s['gate_closed']})
+    return jsonify({'result': output})
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=1234)
